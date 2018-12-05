4c11bb37e13e84b0f8c83848ef5fc5b45b06b114
kojdm <kojdm@bitbucket.org>
2018-12-06 03:35:00 +0800

    added intervention/image to dependencies

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -222,6 +222,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
