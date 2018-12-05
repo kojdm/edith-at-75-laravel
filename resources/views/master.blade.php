c9d57a884fb15a3dd40a73740fdf3675f6303793
kojdm <kojdm@bitbucket.org>
2018-12-06 01:17:07 +0800

    added description meta tag

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A website for Edith's 75th birthday. Upload images and leave messages for her to see.">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta id="token" name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
